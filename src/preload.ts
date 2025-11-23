58580eeb68a442822064e7ccbc1deaca38de78c4
Hephzibah Wilson <hephzibah.p.wilson@gmail.com>
2025-11-23 10:33:23 +0000

    Rejiggering back

--- src/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge } from "electron";
-import path from "path";
-
-// Expose the asset path to the renderer process
-contextBridge.exposeInMainWorld("assetPath", (file: string) => {
-	// In production, assets are in the dist folder
-	return `file://${path.join(__dirname, "assets", file)}`;
-});
